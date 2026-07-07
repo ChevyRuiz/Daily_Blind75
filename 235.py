514a5271d9dfadbb1ed48197317439b27dd79c9f
ChevyRuiz <78135824+ChevyRuiz@users.noreply.github.com>
2026-07-06 21:01:35 -0400

    Add lowestCommonAncestor method for binary tree
    Implement lowestCommonAncestor method to find the LCA of two nodes in a binary tree.

---
+++ 235.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def find(node, val, pathStack):
+            if not node:
+                return
+
+            pathStack.append(node)
+            if val < node.val:
+                find(node.left, val, pathStack)
+            elif val > node.val:
+                find(node.right, val, pathStack)
+            else:
+                return
+
+        pathP = []
+        pathQ = []
+        find(root, p.val, pathP)
+        find(root, q.val, pathQ)
+
+        while pathP:
+            lastElement = pathP.pop()
+            if lastElement in pathQ:
+                return lastElement
+        return None
