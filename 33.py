97f42b693ffb7f602d3b13709f163b9a0da236c5
ChevyRuiz <78135824+ChevyRuiz@users.noreply.github.com>
2026-08-18 13:07:45 -0400

    Implement binary search in rotated sorted array

---
+++ 33.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        
+        min_i = l
+        if min_i == 0:
+            l, r = 0, len(nums) - 1
+        elif target >= nums[0] and target <= nums[min_i - 1]:
+            l, r = 0, min_i - 1
+        else:
+            l, r = min_i, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
